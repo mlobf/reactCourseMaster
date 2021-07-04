0b24d403e79d05dd2c6af2ac38e9d4fe3ec98749
mlobf <marcoslemeborbafilho@gmail.com>
2021-07-04 20:31:02 -0300

    How to change States with React Lesson 25

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// The imports goes here.
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+// Just a functions that returns a html.
+//
+/*
 function App() {
   return (
     <div className="App">
@@ -20,6 +25,32 @@ function App() {
       </header>
     </div>
   );
+}
+*/
+class App extends Component {
+  constructor() {
+    // The super calls the constructor method at Component.
+    //  now is possible use 'this.'
+    super();
+    this.state = {
+      string: "Hello Marcos Leme",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+          <button
+            onClick={() => this.setState({ string: "Hello Borba Filho" })}
+          >
+            Change the text
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
